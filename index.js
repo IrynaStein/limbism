5eda2377fcab884999646e4082d89599b3c890e4
Iryna Stein <isophiestein@gmail.com>
2021-04-13 12:07:25 -0600

    Some text changes

---
+++ index.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Limbism introductory website</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <h1>LIMBISM</h1>
+  </body>
+</html> 
