2987b5969523162be77bf96fbece6c0e2b4205d7
Iryna Stein <isophiestein@gmail.com>
2021-04-14 12:51:49 -0600

    Finished website

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
-const colorChange = document.getElementById('logo');
-colorChange.addEventListener('click', function() {
-  alert('I was clicked!');
+let imageChange = document.getElementById('orange-m');
+imageChange.addEventListener('mouseover', function() {
+  imageChange.src = "images/PinkMushroom.png";
 });
+
+imageChange.addEventListener('mouseout', function() {
+    imageChange.src = "images/OrangeMushroom.png";
+  });
+
+  let img1 = document.getElementById('img1')
+  let img2 = document.getElementById('img2')
+  let img3 = document.getElementById('img3')
+
+  img1.addEventListener ('mouseover', function(){
+      img1.className = 'artWorks big-image'
+  });
+  img1.addEventListener ('mouseout', function(){
+    img1.className = 'artWorks small-image'
+});
+
+img2.addEventListener ('mouseover', function(){
+    img2.className = 'artWorks big-image'
+});
+img2.addEventListener ('mouseout', function(){
+  img2.className = 'artWorks small-image'
+});
+
+img3.addEventListener ('mouseover', function(){
+    img3.className = 'artWorks big-image'
+});
+img3.addEventListener ('mouseout', function(){
+  img3.className = 'artWorks small-image'
+});
+
+let imageChange2 = document.getElementById('green-m');
+imageChange2.addEventListener('mouseover', function() {
+  imageChange2.src = "images/PurpleMushroom.png";
+});
+
+imageChange2.addEventListener('mouseout', function() {
+    imageChange2.src = "images/GreenMushroom.png";
+  });
